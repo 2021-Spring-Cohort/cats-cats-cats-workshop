419d779a863ac40932d5a5d68f1c5ceac3738969
Ben Williams <ben@wecancodeit.org>
2021-01-29 11:18:06 -0500

    Initial commit

---
+++ src/main/java/project/Cat.java
@@ -0,0 +1,11 @@
+package project;
+
+public class Cat {
+
+    private String name;
+
+    public Cat(String name){
+
+        this.name = name;
+    }
+}
---
+++ src/test/java/project/CatTest.java
@@ -0,0 +1,11 @@
+package project;
+
+import org.junit.jupiter.api.Test;
+
+public class CatTest {
+    @Test
+    public void catShouldHaveName(){
+        Cat underTest = new Cat("Testy");
+
+    }
+}
